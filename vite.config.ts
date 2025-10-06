04d15f2be98a3eb9fa494625937d38e895f06ad4
markivoramarketers-wq <markivoramarketers@gmail.com>
2025-10-06 13:53:25 +0500

    Update vite.config.ts
    Signed-off-by: markivoramarketers-wq <markivoramarketers@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/markivoramarketers.online/", // 👈 ye line add karni hai (repo name ya domain name)
   server: {
     host: "::",
     port: 8080,
@@ -16,3 +17,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
