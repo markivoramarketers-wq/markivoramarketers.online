c9422d3f1a22cbbaa98a29ecf4d4dcff947701cc
markivoramarketers-wq <markivoramarketers@gmail.com>
2025-10-06 13:55:56 +0500

    Update vite.config.ts
    Signed-off-by: markivoramarketers-wq <markivoramarketers@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/markivoramarketers.online/", // 👈 ye line add karni hai (repo name ya domain name)
+  base: "/markivoramarketers.online/", // 👈 repo name
   server: {
     host: "::",
     port: 8080,
@@ -16,5 +15,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  },
 }));
-
